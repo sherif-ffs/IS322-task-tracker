2a99bf3fbfad15c6f6f493a2d65f85ce9068c3b6
Sherif Elmetwally <sge5@njit.edu>
2020-03-14 00:39:31 -0400

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import axios from 'axios'
 import Tabs from './shared/Tabs'
 import '../styles/app.css'
 
 import initialData from '../initialData'
 
+
 class App extends React.Component {
   state = initialData
   tasks = this.state.tasks
+
+  componentDidMount() {
+    this.getTasks();
+  }
+
+  getTasks() {
+    axios.get(`https://my-json-server.typicode.com/sherif-ffs/IS322-task-tracker/db`)
+      .then(response => {
+        console.log('response: ', response)
+      }).catch (error => {
+        this.setState({ errorMessage: error.message });
+      });
+  }
 
   render() {
 
